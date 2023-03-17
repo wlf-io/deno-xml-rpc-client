5d70de0a108a8e7fc69d3eefe5d3df545d3170d0
WolfGang <wolfgang@wlf.io>
2023-03-17 15:25:34 +0000

    not npm?

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@types/xmlbuilder"
-export {default as xmlbuilder} from "npm:xmlbuilder";
+export {default as xmlbuilder} from "https://esm.sh/xmlbuilder@15.1.1";
 export {Buffer} from "https://deno.land/std@0.180.0/io/buffer.ts";
 export {SAXParser} from 'https://deno.land/x/sax_ts@v1.2.12/src/sax.ts';
 export {decode as decode64, encode as encode64} from 'https://deno.land/std@0.180.0/encoding/base64.ts';
